906397313cb52d60e7597aa929d2cdd7640110c0
jargalan <zilkenberg22@gmail.com>
2023-01-11 17:24:09 +0800

    zas

--- client/script.js
+++ client/script.js
@@ -84,7 +84,7 @@ const handleSubmit = async (e) => {
     // messageDiv.innerHTML = "..."
     loader(messageDiv);
     // https://chatgpt-q8lw.onrender.com
-    const response = await fetch("http://localhost:5000", {
+    const response = await fetch("https://chatgpt-q8lw.onrender.com", {
         method: "POST",
         headers: {
             "Content-Type": "application/json",
